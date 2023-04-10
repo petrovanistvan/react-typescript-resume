df56695f3691a12b1aac83da727b075ba2c7bdb1
Istvan Petrovan <petrovanistvan@gmail.com>
2023-04-10 21:23:27 +0200

    defined props

---
+++ src/Select.tsx
@@ -0,0 +1,15 @@
+type SelectOption = {
+    label: string
+    value: string
+}
+
+type SelectProps = {
+    options: SelectOption[]
+    value?: SelectOption // optional field
+    onChange: (value: SelectOption | undefined) => void
+    
+}
+
+export function Select({ value, onChange, options }: SelectProps) {
+
+}
